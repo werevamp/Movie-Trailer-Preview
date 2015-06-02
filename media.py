5784288bad8d1582afd9159cb2ff7226b095133f
Albert De Guzman <adeguzman@launchbrigade.com>
2015-06-02 12:48:31 -0700

    first commit

---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie():
+
+    def __init__(self, movie_title, movie_storyline, movie_poster, youtube_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = youtube_trailer
